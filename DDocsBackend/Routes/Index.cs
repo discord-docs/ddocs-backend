ca27e1933e93704380b02155d17c3aeb07cf90e5
James Walston <officialdeltadev@gmail.com>
2022-01-29 19:37:36 -0500

    fix: return / as 200 OK.

--- DDocsBackend/Routes/Index.cs
+++ DDocsBackend/Routes/Index.cs
@@ -5,9 +5,6 @@ public class Index : RestModuleBase
     [Route("/", "GET")]
     public Task<RestResult> ExecuteIndexAsync()
     {
-        Response.Redirect("uri");
-
-        return Task.FromResult(new RestResult(300));
-
+        return Task.FromResult(RestResult.OK);
     }
 }
