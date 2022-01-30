06f5b79501b86ed413951ebe2f0df1222f670170
quin lynch <lynchquin@gmail.com>
2022-01-29 23:11:06 -0400

    chore: ef migration

---
+++ DDocsBackend.Database/Migrations/20220130030953_SummaryContent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DDocsBackend.Data.Migrations
+{
+    public partial class SummaryContent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Summaries",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Summaries",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Summaries");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Summaries");
+        }
+    }
+}
