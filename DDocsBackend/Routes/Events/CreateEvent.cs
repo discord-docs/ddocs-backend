20324da162c91b39f05cf5a798f96b055f4c9c72
Quin Lynch <lynchquin@gmail.com>
2022-02-10 23:14:35 -0400

    fix: require admin for creating test events

--- DDocsBackend/Routes/Events/CreateEvent.cs
+++ DDocsBackend/Routes/Events/CreateEvent.cs
@@ -9,6 +9,7 @@ namespace DDocsBackend.Routes.Events
     public class CreateEvent : RestModuleBase
     {
         [Route("/events", "POST")]
+        [RequireAdmin]
         public async Task<RestResult> CreateEventAsync()
         {
             await DataAccessLayer.CreateEventAsync();
